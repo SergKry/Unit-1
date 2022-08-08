c1aa4e8acdcd48cf41408b365cfbdb414c00a674
SergKry <necroastral@mail.ru>
2022-08-08 21:18:52 +0400

    исправление испорта assert

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,7 +3,7 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
